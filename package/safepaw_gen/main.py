f666af517cf15b9adaecbdaac0c9f916572c1f45
omar-rifai <omar.void@gmail.com>
2026-09-24 09:43:54 +0200

    Remove article-only datasets

--- package/safepaw_gen/main.py
+++ package/safepaw_gen/main.py
@@ -1,12 +1,10 @@
 import typer
 from typing import Literal
 from .mappers.datasets_mappers.maternities_serializer import serialize_maternities
-from .mappers.datasets_mappers.ptgpth_serializer import serialize_ptgpth
-from .mappers.datasets_mappers.burdett_serializer import serialize_burdett
 
 
 
-def cli(instance : Literal["maternities", "ptgpth","burdett"] = typer.Argument(None, help="type of data to generate from (maternities, burdett, ptgpth)"),
+def cli(instance : Literal["maternities"] = typer.Argument(None, help="type of data to generate. Current available options: [maternities]"),
         dep_code: str | None = typer.Argument(None, help="French department code to generate data from, if relevant")):
    
 
@@ -21,13 +19,8 @@ def cli(instance : Literal["maternities", "ptgpth","burdett"] = typer.Argument(N
                 raise typer.BadParameter("dep_code is required for maternities instances")
             serialize_maternities(region_code=None, dep_code=dep_code, save_params=True, global_multiplier_demand=1,
                                   global_multiplier_capacity=1, global_perc_transfers=0)
-        case 'burdett':
-            serialize_burdett(perc_allowed=0, save_params=True)
-        case 'ptgpth':
-            if dep_code is None:
-                            raise typer.BadParameter("dep_code is required for ptgpth instances")
-            serialize_ptgpth(dep_code=dep_code, p_transf=1, p_orth=0, resources_mult=1, quality_requirement=False,
-                             global_multiplier_demand=1, global_multiplier_capacity=1, global_perc_transfers=0, save_params=True)
+        case _:
+             raise typer.BadParameter("Unavailable instance type. aborting.")
     return
 
 
